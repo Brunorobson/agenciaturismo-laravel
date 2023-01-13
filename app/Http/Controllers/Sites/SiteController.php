a0151ed549a719558aaa3b8722d2454b2ecabb59
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2023-01-13 10:53:48 -0300

    Creation of Controller migration and Model

---
+++ app/Http/Controllers/Sites/SiteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Sites;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index(){
+        return view('');
+    }
+
+    public function promotions(){
+        return view('');
+    }
+}
